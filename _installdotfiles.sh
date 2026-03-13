14c9152a6f3fb0e9c367b738612a23698f2d4bff
Colin <152638120+ColinZeDev@users.noreply.github.com>
2026-03-12 18:30:29 -0600

    Update _installdotfiles.sh

--- _installdotfiles.sh
+++ _installdotfiles.sh
@@ -5,7 +5,7 @@ clear
 echo -e "\033[1m\033[38;5;214m+-----------------------------------------+\033[0m"
 echo -e "\033[1m\033[38;5;214m| ####################################### |\033[0m"
 echo -e "\033[1m\033[38;5;214m| ##   ColinZeDev's Dotfiles Installer ## |\033[0m"
-echo -e "\033[1m\033[38;5;214m| ##               v0.0.1              ## |\033[0m"
+echo -e "\033[1m\033[38;5;214m| ##               v0.0.2              ## |\033[0m"
 echo -e "\033[1m\033[38;5;214m| ####################################### |\033[0m"
 echo -e "\033[1m\033[38;5;214m+-----------------------------------------+\033[0m"
 echo
@@ -14,7 +14,6 @@ sudo pacman -Syu \
     bspwm \
     sxhkd \
     xorg-xinit \
-    picom \
     polybar \
     rofi \
     dunst \
@@ -32,8 +31,9 @@ sudo pacman -Syu \
     xorg-xrandr \
     xdg-desktop-portal-gtk \
     xdg-desktop-portal \
+    xdg-utils \
     noto-fonts-emoji \
-    maim \
+    flameshot \
     xclip \
     papirus-icon-theme
 
@@ -42,21 +42,9 @@ git clone https://aur.archlinux.org/yay.git /tmp/yay
 
 yay -S \
     visual-studio-code-bin \
-    cloudflare-warp-bin \
     i3lock-color \
-    papirus-folders
-
-echo
-echo -e "\033[38;5;214mSetting up Cloudflare WARP...\033[0m"
-warp-cli registration new
-
-read -rp "WARP registered. Connect to VPN now? [y/N] " confirm
-if [[ "$confirm" =~ ^[Yy]$ ]]; then
-    warp-cli connect
-    echo -e "\033[38;5;214mWARP connected.\033[0m"
-else
-    echo "Skipping WARP connection. You can connect later with: warp-cli connect"
-fi
+    papirus-folders \
+    picom-ftlabs-git
 
 git clone https://github.com/ColinZeDev/dotfiles.git /tmp/colin_dotfiles
 cp /tmp/colin_dotfiles/.zshrc ~/.zshrc
