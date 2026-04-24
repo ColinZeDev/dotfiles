1cc376e1aeb38f6b63f6cd4e9b89754d38924f7c
Colin <152638120+ColinZeDev@users.noreply.github.com>
2026-04-23 18:50:18 -0600

    Delete .config/bspwm/scripts directory

--- .config/bspwm/scripts/brightness.zsh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-case "$1" in
-    up)   brightnessctl set "${2:-5}%+" ;;
-    down) brightnessctl set "${2:-5}%-" ;;
-esac
-val=$(brightnessctl -m | cut -d, -f4)
-notify-send -t 950 -h string:x-dunst-stack-tag:brightness \
-    -h int:value:$(echo "$val" | tr -d '%') "Brightness" "$val"
